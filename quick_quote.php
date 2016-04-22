458155955edc168fdd6f6c575bbca5577b58b582
Sam Miller <arrgh11@gmail.com>
2016-04-22 11:10:39 -0700

    ugh

--- quick_quote.php
+++ quick_quote.php
@@ -2,7 +2,7 @@
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<script type="text/javascript" src="js/quick_quote.js"></script>
+
 
 </head>
 <title>
